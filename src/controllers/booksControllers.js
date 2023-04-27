1487603162f8f8272630b9707382b52e838ef839
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-04-27 18:40:04 -0300

    fix: databooks

--- src/controllers/booksControllers.js
+++ src/controllers/booksControllers.js
@@ -1,19 +1,21 @@
 import {db} from "../database/database.connection.js"
 //adicionando e pegando os livros pela api
-export async function addBook(books) {   
+export async function addBook() {   
   try {
-    const result = await db.collection("books").insertMany(books);
+    const result = await db.collections("books").insertMany(books);
     console.log(`${result.insertedCount} livros adicionados ao banco de dados`);
   } catch (err) {
     console.log(err, "Erro ao adicionar livros");
   }
 }
+//addBook()
 export async function getAllBooks(req, res){
  
     try{
-        const books = await db.collections("books").find()
+        const books = await db.collections("books").find().toArray()
        res.status(201).send(books)
     }catch(err){
-      console.log(err, "não ha livros")
+      console.log(err, "Não há livros");
+      res.status(500).send(err);
     }
     }
