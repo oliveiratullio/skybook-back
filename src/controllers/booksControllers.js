ae91f185017c79d9b85afbbfecb5f2ae06bf452e
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-05-02 12:26:41 -0300

    feat: product search without tests

--- src/controllers/booksControllers.js
+++ src/controllers/booksControllers.js
@@ -22,3 +22,20 @@ export  async function getAllBooks(req, res){
       res.status(500).send(err);
     }
     }
+    export async function searchOne(req,res){
+      const {search} = req.query
+      try{
+      const products = await db
+      .collection('books')
+      .find({tittle: {$regex: search,$options:'i'}})
+      .toArray()
+      if(!products) return res.status(404).send("Produto não encontrado")
+
+      console.log(products)
+      res.status(200).send(products)
+      }catch(err){
+        console.log(err)
+        res.status(500).send("Erro interno do servidor ")
+
+      }
+    }
